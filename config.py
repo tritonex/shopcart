e9f072187082f3ab2e5c30a51587fcebeb2a30fe
thohu55 <th.thomashuber@gmail.com>
2024-12-02 14:24:02 +0100

    -

--- config.py
+++ config.py
@@ -1,20 +1,10 @@
 import os
-from dotenv import load_dotenv
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv()
 
 class Config:
-    # Basis-Konfiguration
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    
-    # Datenbank-Konfiguration
+    # Datenbank-URL konfigurieren
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
-    
-    # Falls keine DATABASE_URL gesetzt ist, SQLite als Fallback
-    if not SQLALCHEMY_DATABASE_URI:
-        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-    
+        
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev'
